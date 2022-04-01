a6138fd7a595de06c5752f27e80cb0283e43ded5
Xan Surnamehere <57809064+devxan@users.noreply.github.com>
2022-04-01 08:44:45 -0700

    Create animatedThumbnails.js

---
+++ bookmarklets/animatedThumbnails.js
@@ -0,0 +1,1 @@
+javascript:x=document.createElement("script");x.src="//raw.githubusercontent.com/devxan/scratchadv/main/assets/animatedThumbnails.js"; document.getElementsByTagName("head")[0].appendChild(x);

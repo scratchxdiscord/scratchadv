2601e5ba8d6acd9c8b4245bb1f147e16ec649427
Xan <57809064+devxan@users.noreply.github.com>
2022-12-04 18:33:29 -0800

    updated to new owner username

--- bookmarklets/animatedThumbnails.js
+++ bookmarklets/animatedThumbnails.js
@@ -1,1 +1,1 @@
-javascript:x=document.createElement("script");x.src="https://raw.githubusercontent.com/devxan/scratchadv/main/assets/animatedThumbnails.js"; document.getElementsByTagName("head")[0].appendChild(x);
+javascript:x=document.createElement("script");x.src="https://raw.githubusercontent.com/scratchxdiscord/scratchadv/main/assets/animatedThumbnails.js"; document.getElementsByTagName("head")[0].appendChild(x);
